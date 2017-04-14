ca71a6f47c75d71b9d33ef891f3f08e448f9a346
liu_p <liu_p@jointwisdom.cn>
2017-04-14 12:15:08 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # zabbix-alert-email
-邮件报警
+### zabbix 设置
+1. zabbix设置报警脚本存放位置
+```bash
+mkdir /etc/zabbix/zabbix_alert
+
+vim /etc/zabbix/zabbix_server.conf
+AlertScriptsPath=/etc/zabbix/zabbix_alert
+
+systemctl restart zabbix-server
+```
+
+2. 下载邮件脚本
+```bash
+git clone https://github.com/smallasa/zabbix-alert-email.git
+cp zabbix-alert-email/sendmail.py /etc/zabbix/zabbix_alert
+chmod +x /etc/zabbix/zabbix_alert/sendmail.py
+```
+
+3. 添加示警媒介
+```text
+Administration --> Media types --> create media types
+```
+![微信报警媒介](img/邮件报警媒介.png)
+
+5. 为用户添加示警媒介
+```text
+Administration --> Users --> User group --> Create user
+```
+![用户报警媒介](img/用户报警媒介.png)
+
+6. 设置Action
+```text
+Configuration --> Actions --> Event Sources(Triggers) --> Create Action
+```
+![Tiggers.action](img/Tiggers.action.png)
+![Tiggers.operations](img/Tiggers.operations.png)
+![Tiggers.recovery.operation](img/Tiggers.recovery.operation.png)
---
+++ sendmail.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+import smtplib
+from email.mime.text import MIMEText
+import sys
+
+def send_email(email_content, to_email , email_title):
+    msg = MIMEText( email_content, _subtype = 'html', _charset = 'utf8')
+    msg['From'] = 'sos <' + user_name + '>'
+    msg['Subject'] = '%s' % email_title
+
+    try:
+        s = smtplib.SMTP_SSL(smtp_server,465)
+
+        s.login(user_name,user_passwd)
+
+        s.sendmail(user_name,to_email,msg.as_string())
+
+        s.close()
+    except Exception as e:
+        print 'Exception: ', e
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        sys.exit(0)
+
+    smtp_server = 'smtp.exmail.qq.com'
+    user_name   = 'pengliu@brandwisdom.cn'
+    user_passwd = 'P@ssw0rd`'
+
+    to_email_address = sys.argv[1].split(",")
+    to_message_head  = sys.argv[2]
+    to_message_body  = sys.argv[3]
+
+    send_email(to_message_body,to_email_address,to_message_head)
